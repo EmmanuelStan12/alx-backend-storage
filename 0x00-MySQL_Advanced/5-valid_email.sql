e81785bfe5cb2b3a41576d105064dde71eb4fb14
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-10-23 11:06:48 +0100

    task: Email validation sent

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- Task 4: Buy buy buy
+DELIMITER $$
+CREATE TRIGGER `user_BUPD`
+BEFORE UPDATE 
+ON `users` FOR EACH ROW
+BEGIN
+IF NEW.email != OLD.email
+THEN
+SET NEW.valid_email = 0;
+END IF;
+END $$
+DELIMITER ;
