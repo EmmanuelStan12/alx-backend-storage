d85a142d6810066b976bceb72d8d3b9ad4f2cac3
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-10-23 10:02:22 +0100

    task: Glam rock

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,2 @@
+-- Task 3: Old school band
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan FROM metal_bands WHERE FIND_IN_SET('Glam rock', IFNULL(style, '')) > 0 ORDER BY lifespan DESC;
